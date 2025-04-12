6fb7f649254fb0c67e7b1a054f0c7155d06d32fb
Mark <prusarch@mail.ru>
2025-04-12 23:02:12 +0300

    add db connection

---
+++ database.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from typing import Annotated, Optional, List
+
+from sqlalchemy import Integer, func, ARRAY, String
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+from config import settings
+
+DATABASE_URL = settings.get_db_url()
+
+engine = create_async_engine(url=DATABASE_URL)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+#     Декоратор
+def connection(method):
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                return await method(*args,session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()
+                raise e
+            finally:
+                await session.close()
+
+    return wrapper()
+
+
+# Базовый класс для всех моделей
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True  # Класс абстрактный, чтобы не создавать отдельную таблицу для него
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + 's'
+
+
+uniq_str_un = Annotated[str, mapped_column(unique=True)]
+
+array_or_none_an = Annotated[Optional[List[str]], mapped_column(ARRAY(String))]
