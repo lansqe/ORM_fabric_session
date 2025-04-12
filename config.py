3cd3563ddba9da561c4165bba9e28390c3a58ad7
Mark <prusarch@mail.ru>
2025-04-12 23:02:36 +0300

    add db connection/config

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str
 
-    # DATABASE_SQLITE = 'sqlite+aiosqlite:///data/db.sqlite3'
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     )
@@ -17,6 +16,7 @@ class Settings(BaseSettings):
     def get_db_url(self):
         return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
                 f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+
 
 
 settings = Settings()
