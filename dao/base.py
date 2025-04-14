cdf75bfbb7c340d9a7ee712e94076be1c2fad982
Mark <prusarch@mail.ru>
2025-04-14 19:27:58 +0300

    Добавление DAO

---
+++ dao/base.py
@@ -0,0 +1,32 @@
+from typing import List, Any, Dict
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class BaseDAO:
+    model = None # Устанавливается в дочернем классе
+
+    @classmethod
+    async def add(cls, session: AsyncSession, **values):
+        # Добавить одну запись
+        new_instance = cls.model(**values)
+        session.add(new_instance)
+        try:
+            await session.commit()
+        except SQLAlchemyError as e:
+            await session.rollback()
+            raise e
+        return new_instance
+
+
+    @classmethod
+    async def add_many(cls, session: AsyncSession, instances: List[Dict[str, Any]]): # Принимает список словарей
+        # Добавить N-количество записей
+        new_instance = [cls.model(**values) for values in instances]
+        session.add_all(new_instance)
+        try:
+            await session.commit()
+        except SQLAlchemyError as e:
+            await session.rollback()
+            raise e
+        return new_instance
+
---
+++ dao/dao.py
@@ -0,0 +1,15 @@
+from dao.base import BaseDAO
+from models import User, Profile, Post, Comment
+
+class UserDAO(BaseDAO):
+    model = User
+
+class ProfileDAO(BaseDAO):
+    model = Profile
+
+class PostDAO(BaseDAO):
+    model = Post
+
+class CommentDAO(BaseDAO):
+    model = Comment
+
