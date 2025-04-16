56a88fbbebf335880a965ac60533c19e0bf14385
Lansqe <pisarev.orig@mail.ru>
2025-04-16 20:27:17 +0300

    ref and add new dao_model

--- dao/add_methods_dao.py
+++ dao/add_methods_dao.py
@@ -5,6 +5,8 @@ from database import connection
 from asyncio import run
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from sql_enums import GenderEnum, ProfessionEnum
+
 
 @connection
 async def add_one(user_data: dict, session: AsyncSession):
@@ -20,3 +22,24 @@ async def add_many(users_data: List[dict] , session: AsyncSession):
     user_ilds_list = [user.id for user in new_users]
     print(f"Добавлены новые пользователи с ID: {user_ilds_list}")
     return user_ilds_list
+
+@connection
+async def add_full_user(user_data: dict, session: AsyncSession):
+    new_user = await UserDAO.add_user_with_profile(session=session, user_data=user_data)
+    print(f"Добавлен новый пользователь с ID: {new_user.id}")
+    return new_user.id
+
+user_data_bob = {
+    "username": "bob_smith",
+    "email": "bob.smith@example.com",
+    "password": "bobsecure456",
+    "first_name": "Bob",
+    "last_name": "Smith",
+    "age": 25,
+    "gender": GenderEnum.MALE,
+    "profession": ProfessionEnum.DESIGNER,
+    "interests": ["gaming", "photography", "traveling"],
+    "contacts": {"phone": "+987654321", "email": "bob.smith@example.com"}
+}
+
+run(add_full_user(user_data=user_data_bob))
--- dao/dao_models.py
+++ dao/dao_models.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from .base import BaseDAO
 from models import User, Profile, Post, Comment
 
@@ -5,7 +8,58 @@ from models import User, Profile, Post, Comment
 class UserDAO(BaseDAO):
     model = User
 
+    @classmethod
+    async def add_user_with_profile(cls, session: AsyncSession, user_data: dict) -> User:
+        """
+                Добавляет пользователя и привязанный к нему профиль.
 
+                Аргументы:
+                - session: AsyncSession - асинхронная сессия базы данных
+                - user_data: dict - словарь с данными пользователя и профиля
+
+                Возвращает:
+                - User - объект пользователя
+                """
+        # Создаем пользователя из переданных файлов
+        user = cls.model(
+            username=user_data['username'],
+            email=user_data['email'],
+            password=user_data['password']
+        )
+        session.add(user)
+        await session.flush()  # Чтобы получить user.id для профиля
+
+        # Создаем профиль, привязанный к пользователю
+        profile = Profile(
+            user_id=user.id,
+            first_name=user_data['first_name'],
+            last_name=user_data.get('last_name'),
+            age=user_data.get('age'),
+            gender=user_data['gender'],
+            profession=user_data.get('profession'),
+            interests=user_data.get('interests'),
+            contacts=user_data.get('contacts')
+        )
+        session.add(profile)
+
+        # Один коммит для обеих операций
+        await session.commit()
+
+        return user  # Возвращаем объект пользователя
+    # __________________________________________________
+    @classmethod
+    async def get_all_users(cls, session: AsyncSession):
+        # Запрос для выборки всех пользователей
+        query = select(cls.model)
+
+        # Выполняется запрос и получаем ответ
+        result = await session.execute(query)
+
+        # Извлекаем записи как объекты модели
+        records = result.scalars().all()
+
+        # Возвращаем список всех пользователей
+        return records
 
 class ProfileDAO(BaseDAO):
     model = Profile
@@ -17,4 +71,5 @@ class PostDAO(BaseDAO):
 
 class CommentDAO(BaseDAO):
     model = Comment
+
 
